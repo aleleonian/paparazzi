a51311f85865d6c803a9c144e45c56c7f2a651da
Alejandro Leonian <alejandro.leonian@gmail.com>
2025-02-22 21:52:20 -0500

    we implemented the captcha solving on the user side

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-import { XBot } from "xbot-js";
+import { XBot, XBOTConstants } from "xbot-js";
+import readline from "readline";
+
+// Create an interface for reading user input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const DESIRED_USERNAME = process.argv[2];
 let xbot;
@@ -14,6 +21,35 @@ main();
 
 async function main() {
   xbot = new XBot();
+
+  xbot.on(XBOTConstants.XBotEvents.NOTIFICATION, (data) => {
+    console.log(`✅ XBOTConstants.XBotEvents.NOTIFICATION: ${data}`);
+  });
+
+  xbot.on(XBOTConstants.XBotEvents.LOG, (level, ...messages) => {
+    const logMessage = `[${level.toUpperCase()}] ${messages.join(" ")}`;
+
+    if (level === XBOTConstants.LOG_LEVELS.ERROR) {
+      console.error(logMessage);
+    } else if (level === XBOTConstants.LOG_LEVELS.DEBUG) {
+      console.debug(logMessage);
+    } else if (level === XBOTConstants.LOG_LEVELS.WARN) {
+      console.warn(logMessage);
+    } else {
+      console.log(logMessage);
+    }
+  });
+
+  xbot.on(XBOTConstants.XBotEvents.WAIT_FOR_USER_ACTION, (message) => {
+    console.log("\n🛑 X requires user intervention: " + message);
+    console.log("Please solve the captcha, then press Enter to continue...");
+
+    rl.once("line", () => {
+      console.log("✅ Continuing execution...");
+      xbot.emit(XBOTConstants.XBotEvents.CONTINUE);
+    });
+  });
+
   let savedLastTweetUrlPath;
 
   let initResponse = await xbot.init();
@@ -40,8 +76,6 @@ async function main() {
     10000
   );
 
-  console.log("tweets->", JSON.stringify(tweets));
-
   if (!tweets?.length) {
     if (await xbot.findTextInPage("This account doesn’t exist")) {
       return exitApp("Friend, that username does not exist", -1);
@@ -51,10 +85,8 @@ async function main() {
 
   const username = xbot.getTweetAuthor(lastTweet);
 
-  if (username !== DESIRED_USERNAME) {
+  if (username.toLocaleLowerCase() !== DESIRED_USERNAME.toLocaleLowerCase()) {
     return exitApp("Are you sure that username is correct?", -1);
-  } else {
-    console.log("username->", username);
   }
 
   savedLastTweetUrlPath = xbot.getTweetUrlPath(lastTweet);
